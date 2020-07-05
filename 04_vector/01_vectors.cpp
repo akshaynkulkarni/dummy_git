02208f196025ec0ffacb122256a8470af8523597
Akshay Narahari Kulkarni <akshaynkulkarni@gmail.com>
2020-07-05 12:54:32 +0530

    01_vectors: Add basic demos on string based vectors
    1. Sort
    2. Count given literal
    
    Signed-off-by: Akshay Narahari Kulkarni <akshaynkulkarni@gmail.com>

--- 04_vector/01_vectors.cpp
+++ 04_vector/01_vectors.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
 	vector<int> vecti;
+	vector<string> vectstr;
 
 	//read console input of numbers and store in a vector
 	cout << " Hey!! Please enter 10 numbers: " << endl;
@@ -18,5 +21,41 @@ int main()
 	for(int element:vecti) {
 		cout << element << "  ";
 	}
+	cout << endl;
+
+	// read strings
+	cout << "Enter 3 strings: " << endl;
+	for(int i = 0; i<3; i++) {
+		string input;
+		cin >> input;
+		vectstr.push_back(input);
+	}
+
+	cout << "You entered :" << endl;
+	for(string element:vectstr) {
+		cout << element << " ";
+	}
+	cout << endl;
+
+	for(unsigned int i = 0; i < vectstr.size(); i++)
+		cout << vectstr[i] + " ";
+	cout << endl;
+
+	for(auto i = vectstr.begin(); i != vectstr.end(); i++)
+		cout << *i + " ";
+	cout << endl;
+
+	// Count a given char and sort
+	cout << "Number of 'b' in the string vector is: ";
+	cout << count(vectstr[0].begin(), vectstr[0].end(), 'b');
+	cout << endl;
+
+	sort(vectstr.begin(), vectstr.end());
+        cout << "sorted result:" << endl;
+	for(string element:vectstr) {
+		cout << element << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
